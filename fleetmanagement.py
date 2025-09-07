03981912b21b212460a6c58aee9045db35fc288f
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2025-09-07 17:05:15 +0600

    Add files via upload

---
+++ fleetmanagement.py
@@ -0,0 +1,30 @@
+class comapany:
+    def __init__(self,name,address,):
+        self.name=name
+        self.bus=[]
+        self.route=[]
+        self.drivers=[]
+        self.counter=[]
+        self.manager=[]
+        self.supervisor=[]
+        self.fare=[]
+
+class Driver:
+    def __init__(self,name,license,age):
+        self.license=license
+        self.name=name
+        self.age=age
+
+
+class counter:
+    def __init__(self):
+        pass
+    def purches_a_ticket(self,start,destination):
+        pass
+
+class Passenger:
+    pass
+
+class Supervisor:
+    pass
+riponmia=Driver('ripon','ok',34)
